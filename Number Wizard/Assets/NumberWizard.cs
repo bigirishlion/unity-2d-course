6da0cf23fdda3cbb5a41664e2058ea40382f4168
DESKTOP-86LCVTM\BigIrishLion <afmckinney@gmail.com>
2018-08-09 21:25:26 -0700

    11. Responding to player input

--- Number Wizard/Assets/NumberWizard.cs
+++ Number Wizard/Assets/NumberWizard.cs
@@ -6,11 +6,30 @@ public class NumberWizard : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("Hello World!");
+        int max = 1000;
+        int min = 1;
+
+        Debug.Log("Welcome to number wizard, yo");
+        Debug.Log("Pick a number, don't tell me what it is...");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("You pressed up!");
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("You pressed down!");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("You pressed enter!");
+        }
 	}
 }
