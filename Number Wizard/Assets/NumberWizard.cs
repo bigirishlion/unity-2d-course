641f618b9159afab7d36aa323c56a4add8af5071
DESKTOP-86LCVTM\BigIrishLion <afmckinney@gmail.com>
2018-08-09 21:45:19 -0700

    Calculate Guess Variable

--- Number Wizard/Assets/NumberWizard.cs
+++ Number Wizard/Assets/NumberWizard.cs
@@ -15,6 +15,10 @@ public class NumberWizard : MonoBehaviour {
         Debug.Log("Pick a number, don't tell me what it is...");
         Debug.Log("The highest number you can pick is: " + max);
         Debug.Log("The lowest number you can pick is: " + min);
+        Debug.Log("Tell me if you number is higher or lower than " + guess);
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+
+        max = max + 1;
 	}
 	
 	// Update is called once per frame
@@ -22,19 +26,19 @@ public class NumberWizard : MonoBehaviour {
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("You pressed up!");
             min = guess;
-            Debug.Log(min);
+            guess = (max + min) / 2;
+            Debug.Log("Is it higher or lower than..." + guess);
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("You pressed down!");
             max = guess;
-            Debug.Log(max);
+            guess = (max + min) / 2;
+            Debug.Log("Is it higher or lower than..." + guess);
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("You pressed enter!");
+            Debug.Log("I'm Amazing!");
         }
 	}
 }
