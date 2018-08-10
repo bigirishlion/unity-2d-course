0cf4c1ef5f76ec8f981df2f37e03ef249621c5f7
DESKTOP-86LCVTM\BigIrishLion <afmckinney@gmail.com>
2018-08-09 22:01:06 -0700

    15. Functions and Encapsulation

--- Number Wizard/Assets/NumberWizard.cs
+++ Number Wizard/Assets/NumberWizard.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
 
-    int max = 1000;
-    int min = 1;
-    int guess = 500;
+    int max;
+    int min;
+    int guess;
 
 	// Use this for initialization
 	void Start ()
     {
+        StartGame();
+	}
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
         Debug.Log("Welcome to number wizard, yo");
         Debug.Log("Pick a number, don't tell me what it is...");
         Debug.Log("The highest number you can pick is: " + max);
         Debug.Log("The lowest number you can pick is: " + min);
         Debug.Log("Tell me if you number is higher or lower than " + guess);
         Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
-
         max = max + 1;
-	}
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -27,18 +35,23 @@ public class NumberWizard : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             min = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Is it higher or lower than..." + guess);
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             max = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Is it higher or lower than..." + guess);
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("I'm Amazing!");
+            StartGame();
         }
-	}
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is it higher or lower than..." + guess);
+    }
 }
